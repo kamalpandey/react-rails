79f8f4a8f04412f1da34c2fb3b65e15ef678795d
Kamal Pandey <kpandeyy@gmail.com>
2019-10-28 23:25:30 +0545

    initialize project and added bootstrap jquery and popper.js

---
+++ .history/app/javascript/packs/index_20191028232310.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Hello name="React" />,
+    document.body.appendChild(document.createElement('div')),
+  )
+})
