063ad57fb8f7978a266022ff1a66552266320f45
Kamal Pandey <kpandeyy@gmail.com>
2019-11-10 20:43:43 +0545

    All posts

--- app/javascript/components/Posts.js
+++ app/javascript/components/Posts.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Posts = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('/api/v1/posts').then(response => response.json()).then(data => setPosts(data));
+  }, [])
+
   return(
-    <div>
-      <h2>Posts Page</h2>
+    <div className="container">
+      <h2 className="text-center">All Posts</h2>
+      <div className="row">
+        {posts.map(post => (
+          <div className="col-md-4" key={post.id}>
+            <div className="card">
+              <div className="card-header">
+                {post.title}
+              </div>
+              <div className="card-body">
+                <p className="card-text">{post.description}</p>
+                <Link to={`/posts/${post.id}`} className="btn btn-secondary">Show</Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
